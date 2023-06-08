00d97d1bf343e73a51c7627fc399f6ab867ed018
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-08 15:25:22 +0530

    Create merge_script.sh

---
+++ merge_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Clone the repository
+git clone git@github.com:sharan-vayakkady/conflict.git
+cd conflict
+
+# Checkout the target branch
+git checkout  main
+# Merge the source branch into the target branch
+git merge staging
+
+# Check for conflicts
+if [[ -n $(git ls-files -u) ]]; then
+  # Send Slack notification for merge conflict
+  curl -X POST -H 'Content-type: application/json' --data '{"text":"Merge conflict occurred in the repository: conflict"}' https://hooks.slack.com/services/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
+  
+  # Exit with an error status
+  exit 1
+fi
+
+# Push the changes to the remote repository
+git push origin main
